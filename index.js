a614a4c59fd8e46e5432781e94b3a01219464270
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-10-31 18:31:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+const nestedTarget = () => document.querySelector('#nested .target')
+
+const deepestChild = () => document.querySelector('#grand-node div div div div')
+
+function increaseRankBy(n) {
+  const list = [...document.querySelectorAll('.ranked-list')]
+  for (const item of list) {
+    for (const number of [...item.children]) {
+      number.innerHTML = parseInt(number.innerHTML) + n
+    }
+  }
+}
