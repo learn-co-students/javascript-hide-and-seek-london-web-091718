b801d1a8f306900b333691a63149e78f25272434
wimbleclaw <aisha305@hotmail.co.uk>
2018-11-01 11:41:39 +0000

    Done.

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function deepestChild() {
   return document.querySelector("#grand-node div div div div")
 }
 
-function increaseRankBy() {
+function increaseRankBy(n) {
 
   const lis = document.querySelectorAll(".ranked-list")
 
   for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + 1).toString();
+  lis[i].innerHTML = (i + n).toString();
   }
 }
