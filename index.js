afea15db1a8492ebaa6c23370a61ae6de767c178
Ryan <ryangeorgebarker0@gmail.com>
2018-11-01 15:51:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list')
+  const firstList = lists[0]
+  const secondList = lists[1]
+  let childrenFirst = firstList.children
+
+  for (let i = 0; i < childrenFirst.length; i++) {
+    childrenFirst[i].innerHTML = (parseInt(childrenFirst[i].innerHTML) + n)
+  }
+
+   let childrenSecond = secondList.children
+
+  for (let i = 0; i < childrenSecond.length; i++) {
+    childrenSecond[i].innerHTML = (parseInt(childrenSecond[i].innerHTML) + n)
+  }
+}
+
+
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
