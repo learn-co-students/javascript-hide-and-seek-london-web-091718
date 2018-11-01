7f345331bd8774176b623aff2a72c6d73933d528
wimbleclaw <aisha305@hotmail.co.uk>
2018-11-01 11:40:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector) {
+return  document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(".target")
+}
+
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div")
+}
+
+function increaseRankBy() {
+
+  const lis = document.querySelectorAll(".ranked-list")
+
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+  }
+}
